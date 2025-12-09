47058d166428e06f9faac2dde83a14bd40c8c88d
NoHacking <dancntt7@gmail.com>
2025-12-09 21:36:41 +0700

    update: checkpoint

--- core/mouse_controller.py
+++ core/mouse_controller.py
@@ -1,35 +1,42 @@
 from .scroll_manager import set_scroll_direction
 from .scroll_manager import set_scroll_active
-from pynput.mouse import Listener, Button
-from pynput.keyboard import Controller, Key
 
-# Khởi tạo keyboard controller
-keyboard = Controller()
+from pynput.mouse import Listener
+from pynput.mouse import Button
+
+from pynput.keyboard import Key
+
+import pynput.keyboard
+import pynput.mouse
 
 
-def on_click(x, y, button, pressed):
+keyboard = pynput.keyboard.Controller()
+mouse =  pynput.mouse.Controller()
+
+def mouse_click(x, y, button, pressed):
     if button == Button.x1:
         if pressed:
             set_scroll_active(True)
-            set_scroll_direction(-1)  # Scroll down
+            set_scroll_direction(-1)
         else:
             set_scroll_active(False)
             set_scroll_direction(0)
-    elif button == Button.x2:
+    if button == Button.x2:
         if pressed:
             set_scroll_active(True)
-            set_scroll_direction(1)  # Scroll up
+            set_scroll_direction(1)
         else:
             set_scroll_active(False)
             set_scroll_direction(0)
-    elif button == Button.middle and pressed:
-        # Gửi phím F9 khi nhấn chuột giữa
+
+    if button == Button.middle and pressed:
+        mouse.click(Button.left)
         keyboard.press(Key.f9)
         keyboard.release(Key.f9)
     return True
 
 
 def start_listener():
-    listener = Listener(on_click=on_click)
+    listener = Listener(on_click=mouse_click)
     listener.start()
     return listener
